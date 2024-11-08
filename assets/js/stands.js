c865f31c80759a78c9bdf621c1d6a552967f2359
mahimka1321 <xakepok7771@mail.ru>
2024-11-08 09:35:26 +0300

    com9

--- assets/js/stands.js
+++ assets/js/stands.js
@@ -3,40 +3,65 @@ let standTwo = document.getElementById('standTwo');
 let standThree = document.getElementById('standThree');
 
 let cardST1 = document.getElementById('cardST1');
-let cardST2 = document.getElementById('cardST2');
-let cardST3 = document.getElementById('cardST3');
+
+let photoSlid1 = document.getElementById('photoSlid1');
+let photoSlid2 = document.getElementById('photoSlid2');
+let photoSlid3 = document.getElementById('photoSlid3');
+let photoSlid4 = document.getElementById('photoSlid4');
+let photoSlid5 = document.getElementById('photoSlid5');
+
+let nameStand = document.getElementById('nameStand');
+let aboutStand = document.getElementById('aboutStand');
 
 let buttonClean1 = document.getElementById('buttonClean1');
-let buttonClean2 = document.getElementById('buttonClean2');
-let buttonClean3 = document.getElementById('buttonClean3');
 
 
 let wrapperCard = document.getElementById('wrapperCard');
 
-standOne.addEventListener("click", function() {
+function opnen() {
     cardST1.classList.remove('noneCard')
     wrapperCard.classList.add('noneCard')
+}
+
+standOne.addEventListener("click", function() {
+    opnen()
+    photoSlid1.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid2.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid3.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid4.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid5.src = 'assets/imges/st2/02_Kluch_01.png';
+
+    nameStand.innerHTML = '<span>Д</span>озор';
+    aboutStand.innerText = 'Это дозор';
+
 });
 
 standTwo.addEventListener("click", function() {
-    cardST2.classList.remove('noneCard')
-    wrapperCard.classList.add('noneCard')
+    opnen()
+    photoSlid1.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid2.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid3.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid4.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid5.src = 'assets/imges/st2/02_Kluch_01.png';
+
+    nameStand.innerHTML = '<span>П</span>арус';
+    aboutStand.innerText = 'Это парус';
 });
 
 standThree.addEventListener("click", function() {
-    cardST3.classList.remove('noneCard')
-    wrapperCard.classList.add('noneCard')
+    opnen()
+    photoSlid1.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid2.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid3.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid4.src = 'assets/imges/st2/02_Kluch_01.png';
+    photoSlid5.src = 'assets/imges/st2/02_Kluch_01.png';
+
+    nameStand.innerHTML = '<span>К</span>люч';
+    aboutStand.innerText = 'Это ключ';
 });
+
 
 buttonClean1.addEventListener("click", function() {
     wrapperCard.classList.remove('noneCard')
     cardST1.classList.add('noneCard')
-});
-buttonClean2.addEventListener("click", function() {
-    wrapperCard.classList.remove('noneCard')
-    cardST2.classList.add('noneCard')
-});
-buttonClean3.addEventListener("click", function() {
-    wrapperCard.classList.remove('noneCard')
-    cardST3.classList.add('noneCard')
 });
