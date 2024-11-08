083d7890cf734780d617220364ddc7b50096a5ed
mahimka1321 <xakepok7771@mail.ru>
2024-11-08 08:32:08 +0300

    com8

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,24 +30,48 @@ toggleBtn.addEventListener("click", function() {
 });
        
 
+let standOne = document.getElementById('standOne')
+let standTwo = document.getElementById('standTwo')
+let standThree = document.getElementById('standThree')
+
+let cardST1 = document.getElementById('cardST1')
+let cardST2 = document.getElementById('cardST2')
+let cardST3 = document.getElementById('cardST3')
+
+let buttonClean1 = document.getElementById('buttonClean1')
+let buttonClean2 = document.getElementById('buttonClean2')
+let buttonClean3 = document.getElementById('buttonClean3')
 
 
-(() => {
-    const activeBtn = document.getElementById('activePopup')
-    const containerPopup = document.getElementById('containerPopup')
+let wrapperCard = document.getElementById('wrapperCard')
 
-    function toggle() { 
-        containerPopup.classList.toggle('container__active_popup'); 
-    }
-    function remove() { 
-        containerPopup.classList.remove('container__active_popup'); 
-    }
+standOne.addEventListener("click", function() {
+    cardST1.classList.remove('noneCard')
+    wrapperCard.classList.add('noneCard')
+})
 
-    document.addEventListener('click', e => {
-        const target = e.target;
-        target === activeBtn ? toggle() : target !== containerPopup ? remove() : false;
-    });
-})();
+standTwo.addEventListener("click", function() {
+    cardST2.classList.remove('noneCard')
+    wrapperCard.classList.add('noneCard')
+})
+
+standThree.addEventListener("click", function() {
+    cardST3.classList.remove('noneCard')
+    wrapperCard.classList.add('noneCard')
+})
+
+buttonClean1.addEventListener("click", function() {
+    wrapperCard.classList.remove('noneCard')
+    cardST1.classList.add('noneCard')
+})
+buttonClean2.addEventListener("click", function() {
+    wrapperCard.classList.remove('noneCard')
+    cardST2.classList.add('noneCard')
+})
+buttonClean3.addEventListener("click", function() {
+    wrapperCard.classList.remove('noneCard')
+    cardST3.classList.add('noneCard')
+})
 
 
 const myObject = {
